3354d5d0bb38616d9bcc98ffe1f342dbb8419459
Vishnu Varma <vishvarm@lawn-143-215-113-56.lawn.gatech.edu>
2023-09-22 12:51:21 -0400

    created more connections between screens. Edited playerdata and game activity screen based on user input slightly. Need to fix playerview still

--- app/src/main/java/com/example/cs2340a_team43/models/GameActivity.java
+++ app/src/main/java/com/example/cs2340a_team43/models/GameActivity.java
@@ -1,15 +1,42 @@
 package com.example.cs2340a_team43.models;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.cs2340a_team43.R;
 
 
 
 public class GameActivity extends AppCompatActivity {
+
+    int hp;
+    int choice;
+    ConstraintLayout gameLayout;
+    PlayerView playerView;
+    int screenWidth;
+    int screenHeight;
+    private float playerX, playerY;
+    private TextView hpTextView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        gameLayout = findViewById(R.id.gameLayout);
+        screenWidth = getResources().getDisplayMetrics().widthPixels;
+        screenHeight = getResources().getDisplayMetrics().heightPixels;
+        // Spawn player in middle of screen
+        playerX = screenWidth / 2;
+        playerY = screenHeight / 2;
+        hp = getIntent().getIntExtra("hp", 50);
+        hpTextView = findViewById(R.id.healthTextView);
+        hpTextView.setText("Health: " + hp);
+        choice = getIntent().getIntExtra("choice", 0);
+        //playerView = new PlayerView(this, playerX, playerY, hp, choice);
+        //gameLayout.addView(playerView);
+
+
     }
 }
