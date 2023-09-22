28c40493d6a11dcab9fd70d12b8ebfb73ac8a4a9
Jackson Rupert <jrupert37@gatech.edu>
2023-09-22 14:30:58 -0400

    Fix conflicts, display difficulty on game screen

--- app/src/main/java/com/example/cs2340a_team43/models/GameActivity.java
+++ app/src/main/java/com/example/cs2340a_team43/models/GameActivity.java
@@ -14,6 +14,7 @@ import com.example.cs2340a_team43.R;
 public class GameActivity extends AppCompatActivity {
 
     int hp;
+    String difficulty;
     int choice;
     ConstraintLayout gameLayout;
     PlayerView playerView;
@@ -22,6 +23,7 @@ public class GameActivity extends AppCompatActivity {
     private float playerX, playerY;
     private TextView hpTextView;
     private Button endButton;
+    private TextView difficultyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +39,9 @@ public class GameActivity extends AppCompatActivity {
         hpTextView = findViewById(R.id.healthTextView);
         hpTextView.setText("Health: " + hp);
         choice = getIntent().getIntExtra("choice", 0);
+        difficulty = getIntent().getStringExtra("difficulty");
+        difficultyTextView = findViewById(R.id.difficultyTextView);
+        difficultyTextView.setText("Difficulty: " + difficulty);
         //playerView = new PlayerView(this, playerX, playerY, hp, choice);
         //gameLayout.addView(playerView);
         endButton = findViewById(R.id.endScreenButton);
