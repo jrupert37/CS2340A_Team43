890e2861ef40ff95755cd3fc59c60131bc465e23
Jackson Rupert <jrupert37@gatech.edu>
2023-10-03 22:00:21 -0400

    Add restart button base functionality to take player back to start screen

--- app/src/main/java/com/example/cs2340a_team43/Views/EndScreenActivity.java
+++ app/src/main/java/com/example/cs2340a_team43/Views/EndScreenActivity.java
@@ -1,14 +1,27 @@
 package com.example.cs2340a_team43.Views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cs2340a_team43.R;
 
 public class EndScreenActivity extends AppCompatActivity {
+
+    private Button restartButton;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end);
+
+        restartButton = findViewById(R.id.restartButton);
+        restartButton.setOnClickListener(v -> {
+            Intent intent = new Intent(EndScreenActivity.this, StartScreenActivity.class);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            startActivity(intent);
+            finish();
+        });
     }
 }
