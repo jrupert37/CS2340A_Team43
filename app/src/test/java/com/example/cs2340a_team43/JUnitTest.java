878e502a399aff4b1eb7d5fb43be3f84a88fc3a1
Joseph-Ruth <jruth7@gatech.edu>
2023-10-11 16:23:40 -0400

    Added test for the Leaderboard class being Singelton.

--- app/src/test/java/com/example/cs2340a_team43/JUnitTest.java
+++ app/src/test/java/com/example/cs2340a_team43/JUnitTest.java
@@ -7,6 +7,7 @@ import com.example.cs2340a_team43.Models.Leaderboard;
 import com.example.cs2340a_team43.Models.Player;
 
 import java.util.Calendar;
+import java.util.TimeZone;
 
 public class JUnitTest {
 
@@ -91,5 +92,40 @@ public class JUnitTest {
         assertEquals(70, leaderboard.getMostRecentAttempt().getScore());
 
         leaderboard.clear();
+    }
+
+    // Joseph Test #1
+    public void testLeaderboardSingleton() {
+        Leaderboard leaderboard = Leaderboard.getInstance();
+        leaderboard.addAttempt("Player", 2, Calendar.getInstance(),
+                Calendar.getInstance(TimeZone.getTimeZone("GMT - 3:00")));
+        // Node attempt1 = leaderboard.getMostRecentAttempt();
+        // assertEquals(attempt1, leaderboard.getMostRecentAttempt());
+        assertEquals(leaderboard.getMostRecentAttempt(),
+                leaderboard.get(leaderboard.getSize() - 1));
+        // Last element in the Array in the Leaderboard class should be the mostRecentAttempt.
+
+        Leaderboard leaderboard2 = Leaderboard.getInstance();
+        // Node attempt2 = leaderboard2.getMostRecentAttempt();
+        // assertEquals(attempt2, leaderboard2.getMostRecentAttempt());
+        leaderboard2.getMostRecentAttempt().setName("Player2");
+        leaderboard2.getMostRecentAttempt().setScore(5);
+        leaderboard2.getMostRecentAttempt().setStartTime(Calendar.getInstance(TimeZone.getTimeZone
+                ("GMT - 1:00")));
+        leaderboard2.getMostRecentAttempt().setEndTime(Calendar.getInstance(TimeZone.getTimeZone
+                ("GMT - 4:00")));
+        assertEquals(leaderboard2.getMostRecentAttempt(),
+                leaderboard2.get(leaderboard2.getSize() - 1));
+
+        assertEquals(leaderboard2.getMostRecentAttempt(), leaderboard.getMostRecentAttempt());
+        // The next 4 tests below are for safety, they should be covered in the test above.
+        assertEquals(leaderboard2.getMostRecentAttempt().getName(),
+                leaderboard.getMostRecentAttempt().getName());
+        assertEquals(leaderboard2.getMostRecentAttempt().getScore(),
+                leaderboard.getMostRecentAttempt().getScore());
+        assertEquals(leaderboard2.getMostRecentAttempt().getStartTime(),
+                leaderboard.getMostRecentAttempt().getStartTime());
+        assertEquals(leaderboard2.getMostRecentAttempt().getEndTime(),
+                leaderboard.getMostRecentAttempt().getEndTime());
     }
 }
