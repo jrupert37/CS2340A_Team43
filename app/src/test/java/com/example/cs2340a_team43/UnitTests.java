2a5416eb5444aec5d87e13b3b6c0ec001002fb86
laurennpak <lauren09pak@gmail.com>
2023-10-11 15:49:32 -0400

    test whether setter and getter for names words

--- app/src/test/java/com/example/cs2340a_team43/UnitTests.java
+++ app/src/test/java/com/example/cs2340a_team43/UnitTests.java
@@ -1,9 +1,10 @@
 package com.example.cs2340a_team43;
+//package com.journaldev.mockito;
 import org.junit.Test;
 import static org.junit.Assert.assertThrows;
 import android.content.Context;
 import com.example.cs2340a_team43.Models.Player;
-import com.example.cs2340a_team43.Models.TempScoreFeature;
+import com.example.cs2340a_team43.Views.TempScoreFeature;
 import static org.junit.Assert.*;
 import android.os.Handler;
 import android.os.Looper;
@@ -13,44 +14,32 @@ public class UnitTests {
     private Context context;
     //Lauren Test #1
     @Test
-    public void testNameEmptyCheck() {
+    public void testNameNullCheck() {
         Player player = Player.getInstance();
         assertThrows(IllegalStateException.class, () -> {
             player.setName(null);
         });
     }
     @Test
-    public void testNameNullCheck() {
+    public void testNameEmptyCheck() {
         Player player = Player.getInstance();
-        assertTrue(player.getName() == null || player.getName().trim().isEmpty());
+        assertThrows(IllegalStateException.class, () -> {
+            player.setName("");
+        });
     }
-/*
     @Test
-    public void scoreSet() {
-        TempScoreFeature temp = new TempScoreFeature();
-        int initialScore = temp.getHealthScore();
-        for (int i = 0; i < 5; i++) {
-            temp.onCreate();
-            int updatedScore = temp.getHealthScore();
-            assertTrue("True", updatedScore < initialScore);
-            initialScore = updatedScore;
+    public void testSetName() {
+        Player player = Player.getInstance();
+        player.setName("Bob");
+        String name = player.getName();
+        assertEquals("Bob", name);
+    }
+    @Test
+    public void testGetName() {
+        Player player = Player.getInstance();
+        player.setName("John");
+        String name = player.getName();
+        assertEquals("John", name);
 
-            final Object lock = new Object();
-            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    synchronized (lock) {
-                        lock.notify();
-                    }
-                }
-            }, 1000);
-            try {
-                synchronized (lock) {
-                    lock.wait();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }*/
+    }
 }
