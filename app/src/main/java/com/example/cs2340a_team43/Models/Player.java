b00bddaa005893517d44b97dafe8dcfb9a5ac0b2
Jackson Rupert <jrupert37@gatech.edu>
2023-10-20 11:06:19 -0400

    Implement strategy pattern for movement, add setters for initialization, created player bitmap

--- app/src/main/java/com/example/cs2340a_team43/Models/Player.java
+++ app/src/main/java/com/example/cs2340a_team43/Models/Player.java
@@ -1,20 +1,27 @@
 package com.example.cs2340a_team43.Models;
 
-public class Player {
-    //private float x;
-    //private float y;
-    private double damage;
-    private int hp;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import com.example.cs2340a_team43.Models.MovementBehavior.MovementDirection;
+import android.util.Pair;
 
+
+public class Player extends Character {
     private static Player player;
-    private String name;
+    private String playerName;
+    private Context context;
+    private int hp;
+    private int imageId;
+    private Bitmap bitmap;
 
     private Player() {
-        this.damage = 0.0;
-        //this.y = y;
+        // null constructor, player attributes will be concretely initialized later
         this.hp = 0;
-        //playerImageView = findViewById(R.id.playerImageView);
-        //playerImageView.setImageResource(R.drawable.frowny);
+        xyCoordinates = new Pair<>(0, 0);
+        this.imageId = 0;
+        this.bitmap = null;
+        this.movementBehavior = new WalkMovement();
     }
 
     public static Player getInstance() {
@@ -27,9 +34,19 @@ public class Player {
         return hp;
     }
 
-    /*public void setHP(int hp) {
-        this.hp = hp;
-    }*/
+    public int getX() {
+        // the "first" element of the xyCoordinatesCoordinates pair is the x coordinate
+        return xyCoordinates.first;
+    }
+
+    public int getY() {
+        // the "second" element of the xyCoordinates pair is the y coordinate
+        return xyCoordinates.second;
+    }
+
+    public void setInitialXY(int x, int y) {
+        xyCoordinates = new Pair<>(x, y);
+    }
     public void setHP(String difficulty) {
         if (difficulty.equals("Easy")) {
             hp = 50;
@@ -39,22 +56,45 @@ public class Player {
             hp = 15;
         }
     }
-    public void setDamage(double damage) {
-        this.damage = damage;
-    }
-    public double getDamage() {
-        return damage;
-    }
+
     public String getName() {
-        return name;
+        return playerName;
     }
-    public void setName(String name) {
-        this.name = name;
-        if (name == null || name == "") {
+
+    public void setName(String playerName) {
+        this.playerName = playerName;
+        if (playerName == null || playerName == "") {
             throw new IllegalStateException();
         }
     }
 
+    public void setImageId(int imageId, Context context) {
+        this.imageId = imageId;
+        this.context = context;
+        this.bitmap = BitmapFactory.decodeResource(this.context.getResources(), imageId);
+    }
 
+    public void moveLeft() {
+        updatePosition(MovementDirection.LEFT);
+    }
 
-}
+    public void moveRight() {
+        updatePosition(MovementDirection.RIGHT);
+    }
+
+    public void moveUp() {
+        updatePosition(MovementDirection.UP);
+    }
+
+    public void moveDown() {
+        updatePosition(MovementDirection.DOWN);
+    }
+    private void updatePosition(MovementDirection direction) {
+        this.xyCoordinates = performMove(xyCoordinates, direction);
+        System.out.println("X: " + xyCoordinates.first);
+        System.out.println("Y: " + xyCoordinates.second);
+    }
+    public Bitmap getBitmap() {
+        return this.bitmap;
+    }
+} // Player
