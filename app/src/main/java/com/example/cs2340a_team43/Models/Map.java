d2cd66cc491880f8b0a78062cc161724a5418bd7
Jackson Rupert <jrupert37@gatech.edu>
2023-10-22 18:29:13 -0400

    Add EXIT to MapObjects, edit layout array to match map grid dimensions, set the array based on current floor, create method to scan through blueprint files and fill layout array

--- app/src/main/java/com/example/cs2340a_team43/Models/Map.java
+++ app/src/main/java/com/example/cs2340a_team43/Models/Map.java
@@ -6,29 +6,33 @@ import android.graphics.BitmapFactory;
 
 import com.example.cs2340a_team43.R;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Map {
     public enum Floor {
         FIRST_FLOOR,
         SECOND_FLOOR,
         THIRD_FLOOR
     }
-
     public enum MapObject {
         WALL,
+        EXIT,
         EMPTY
     }
+    //private static Map map;
     private Floor floor;
     private Bitmap floorBitmap;
     private Context context;
     private MapObject[][] roomLayout;
-    private final int rows = 34;
-    private final int cols = 87;
+    private final int rows = 18;
+    private final int cols = 40;
+
     public Map(Context context) {
-        this.context = context;
         this.floor = Floor.FIRST_FLOOR;
-        this.floorBitmap = BitmapFactory.decodeResource(context.getResources(),
-                            R.drawable.firstfloor);
-        roomLayout = getFirstFloorLayout();
+        this.context = context;
+        setFloor(floor);
     }
 
     public Floor getFloor() {
@@ -40,15 +44,18 @@ public class Map {
         switch (floor) {
         case FIRST_FLOOR:
             this.floorBitmap = BitmapFactory.decodeResource(context.getResources(),
-                                R.drawable.firstfloor);
+                                R.drawable.floor1);
+            this.roomLayout = setFloorLayout("first_floor_blueprint.txt");
             break;
         case SECOND_FLOOR:
             this.floorBitmap = BitmapFactory.decodeResource(context.getResources(),
-                                R.drawable.secondfloor);
+                                R.drawable.floor2);
+            this.roomLayout = setFloorLayout("second_floor_blueprint.txt");
             break;
         case THIRD_FLOOR:
             this.floorBitmap = BitmapFactory.decodeResource(context.getResources(),
-                    R.drawable.thirdfloor);
+                    R.drawable.floor3);
+            this.roomLayout = setFloorLayout("third_floor_blueprint.txt");
             break;
         default:
             break;
@@ -57,28 +64,71 @@ public class Map {
     public Bitmap getFloorBitmap() {
         return this.floorBitmap;
     }
-    private MapObject[][] getFirstFloorLayout() {
-        MapObject[][] layout = new MapObject[rows][cols];
-
-        // Populate map borders with walls, everywhere else with empty spaces
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                if (row == 0 || col == 0 || col == 86 || row == 33) {
-                    layout[row][col] = MapObject.WALL;
-                } else {
-                    layout[row][col] = MapObject.EMPTY;
-                }
-            }
-        }
-
-        for (int col = 10; col <= 47; col++) {
-            layout[1][col] = MapObject.WALL;
-        }
-
-        return layout;
-    }
     public MapObject[][] getRoomLayout() {
         return this.roomLayout;
     }
+    private MapObject[][] setFloorLayout(String pathname) {
+        InputStream stream = null;
+        try {
+            stream = this.context.getAssets().open(pathname);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int index = 0;
+        MapObject[][] layout = new MapObject[rows][cols];
+        Scanner streamScanner = new Scanner(stream);
+        while (streamScanner.hasNext()) {
+            int row = index / 40;
+            int col = index % 40;
+            char value = streamScanner.next().charAt(0);
+            if (value == 'W') {
+                layout[row][col] = MapObject.WALL;
+            } else if (value == 'N') {
+                layout[row][col] = MapObject.EMPTY;
+            } else if (value == 'E') {
+                layout[row][col] = MapObject.EXIT;
+            }
+            index++;
+        }
+        return layout;
+    }
 
+    //    private void createFirstFloorLayout() {
+    //        firstFloorLayout = new MapObject[rows][cols];
+    //        for (int row = 0; row < rows; row++) {
+    //            for (int col = 0; col < cols; col++) {
+    //                if (row == 0 || col == 0 || col == 39 || row == 17) {
+    //                    firstFloorLayout[row][col] = MapObject.WALL;
+    //                } else {
+    //                    firstFloorLayout[row][col] = MapObject.EMPTY;
+    //                }
+    //            }
+    //        }
+    //        for (int row = 3; row <= 7; row++) {
+    //            for (int col = 24; col <= 27; col++) {
+    //                firstFloorLayout[row][col] = MapObject.WALL;
+    //            }
+    //        }
+    //        for (int row = 1; row <= 9; row++) {
+    //            firstFloorLayout[row][18] = MapObject.WALL;
+    //        }
+    //        for (int col = 1; col <= 18; col++) {
+    //            if (col != 5 && col != 6) {
+    //                firstFloorLayout[10][col] = MapObject.WALL;
+    //            }
+    //        }
+    //        for (int row = 1; row <= 12; row++) {
+    //            firstFloorLayout[row][33] = MapObject.WALL;
+    //        }
+    //        for (int row = 10; row <= 16; row++) {
+    //            firstFloorLayout[row][23] = MapObject.WALL;
+    //        }
+    //        for (int row = 10; row <= 16; row++) {
+    //            firstFloorLayout[row][28] = MapObject.WALL;
+    //        }
+    //        firstFloorLayout[10][24] = MapObject.WALL;
+    //        firstFloorLayout[10][27] = MapObject.WALL;
+    //        firstFloorLayout[12][29] = MapObject.WALL;
+    //        firstFloorLayout[12][32] = MapObject.WALL;
+    //    }
 } // Map
