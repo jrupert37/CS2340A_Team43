5cf9892eb0c866b4242ce8b81a09d701972a764a
Vishnu Varma <vishvarm@lawn-143-215-115-244.lawn.gatech.edu>
2023-10-25 12:07:27 -0400

    fixed factory errors

--- app/src/main/java/com/example/cs2340a_team43/Models/EnemyFourFactory.java
+++ app/src/main/java/com/example/cs2340a_team43/Models/EnemyFourFactory.java
@@ -1,7 +1,7 @@
 package com.example.cs2340a_team43.Models;
 
 public class EnemyFourFactory extends EnemySpawner {
-    protected Enemy createEnemy() {
-        return new EnemyFour();
+    protected Enemy createEnemy(String difficulty) {
+        return new EnemyFour(difficulty);
     }
 }
