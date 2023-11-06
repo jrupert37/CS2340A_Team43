e459874f99de3925e494253d77e645b5e3c2b32a
Jackson Rupert <jrupert37@gatech.edu>
2023-11-05 23:24:59 -0500

    Add explanatory comments, checkstyle fixes, extend Timer instead of Thread, implement interface useful for starting and stopping the movement pattern

--- app/src/main/java/com/example/cs2340a_team43/Models/EyeballMovement.java
+++ app/src/main/java/com/example/cs2340a_team43/Models/EyeballMovement.java
@@ -1,45 +1,54 @@
 package com.example.cs2340a_team43.Models;
 
 import com.example.cs2340a_team43.ViewModels.EnemyViewModel;
-
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class EyeballMovement extends Thread {
+/*
+ * This pattern of movement is made with the Eyeball enemy in mind, but could be used by
+ * other enemy types.
+ * Movement pattern is on a timer. An enemy with this pattern will move in a random
+ * direction every 1.25 seconds.
+ * The next direction the enemy will move is generated randomly between up, down,
+ * left, and right.
+ * This movement pattern uses the given EnemyViewModel's movement methods, which will
+ * handle wall collisions.
+ */
+public class EyeballMovement extends Timer implements ControllableMovement {
+    private final EnemyViewModel evm;
 
-    //private Enemy enemy;
-    private EnemyViewModel evm;
-
-    public EyeballMovement (EnemyViewModel evm) {
+    public EyeballMovement(EnemyViewModel evm) {
         this.evm = evm;
     }
 
-    @Override
-    public void run() {
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Random rand = new Random();
-                int randomValue = rand.nextInt(4);
-                switch (randomValue) {
-                    case 0:
-                        evm.moveEnemyDown();
-                        break;
-                    case 1:
-                        evm.moveEnemyUp();
-                        break;
-                    case 2:
-                        evm.moveEnemyRight();
-                        break;
-                    case 3:
-                        evm.moveEnemyLeft();
-                        break;
-                }
-            }
-        }, 2000, 2000); // delay 1sec, then execute run() every 1sec til score is 0
+    public void stop() {
+        super.cancel();
     }
 
-
-}
+    public void start() {
+        Random rand = new Random();
+        super.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                int randomValue = rand.nextInt(4);
+                switch (randomValue) {
+                case 0:
+                    evm.moveEnemyDown();
+                    break;
+                case 1:
+                    evm.moveEnemyUp();
+                    break;
+                case 2:
+                    evm.moveEnemyRight();
+                    break;
+                case 3:
+                    evm.moveEnemyLeft();
+                    break;
+                default:
+                    break;
+                }
+            }
+        }, 1000, 1250);
+    }
+} // EyeballMovementPattern
