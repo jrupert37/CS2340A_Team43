10aaa483f0d0e801dc2606dbb119441bbc8a7522
Jackson Rupert <jrupert37@gatech.edu>
2023-11-05 23:11:46 -0500

    Add explanatory comments, checkstyle fixes, change visibility modifiers to private, change performMove to set xyCoordinates instead of returning new a new xy, add getters/setters

--- app/src/main/java/com/example/cs2340a_team43/Models/Character.java
+++ app/src/main/java/com/example/cs2340a_team43/Models/Character.java
@@ -12,6 +12,7 @@ public abstract class Character {
     public Pair<Integer, Integer> performMove(Pair<Integer, Integer> xy, MovementDirection dir) {
         return movementBehavior.move(xy, dir);
     }
+
     public void setMovementBehavior(MovementBehavior behavior) {
         this.movementBehavior = behavior;
     }
