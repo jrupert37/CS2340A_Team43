208947d4c47525c3656c695ff7677c434aa24ab9
Jackson Rupert <jrupert37@gatech.edu>
2023-11-05 23:12:16 -0500

    Add explanatory comments, checkstyle fixes, change visibility modifiers to private, change performMove to set xyCoordinates instead of returning new a new xy, add getters/setters

--- app/src/main/java/com/example/cs2340a_team43/Models/Character.java
+++ app/src/main/java/com/example/cs2340a_team43/Models/Character.java
@@ -1,22 +1,51 @@
 package com.example.cs2340a_team43.Models;
 
 import androidx.core.util.Pair;
-
 import com.example.cs2340a_team43.Models.MovementBehavior.MovementDirection;
 
+/*
+* This is a parent class for any Character type, including player and enemies
+*/
 public abstract class Character {
-    protected MovementBehavior movementBehavior;
-    protected Pair<Integer, Integer> xyCoordinates = new Pair<>(0, 0);
-    public Character() {
+    private MovementBehavior movementBehavior;
+    private Pair<Integer, Integer> xyCoordinates;
+
+    protected Character() {
+        this.movementBehavior = new WalkMovement(); // set to WalkMovement by default
+        this.xyCoordinates = new Pair<>(0, 0);      // set to (0,0) by default
     }
-    public Pair<Integer, Integer> performMove(Pair<Integer, Integer> xy, MovementDirection dir) {
-        return movementBehavior.move(xy, dir);
+
+    //    public Pair<Integer, Integer> performMove(Pair<Integer, Integer> xy, MovementDirection
+    //    dir) {
+    //        return movementBehavior.move(xy, dir);
+    //    }
+
+    public void performMove(MovementDirection dir) {
+        setXyCoordinates(movementBehavior.move(this.xyCoordinates, dir));
     }
 
     public void setMovementBehavior(MovementBehavior behavior) {
         this.movementBehavior = behavior;
     }
+
     public int getSpeed() {
         return this.movementBehavior.getSpeed();
     }
-}
+
+    public int getX() {
+        return this.xyCoordinates.first;
+    }
+
+    public  int getY() {
+        return this.xyCoordinates.second;
+    }
+
+    public void setInitialXY(int x, int y) {
+        this.xyCoordinates = new Pair<>(x, y);
+    }
+
+    private void setXyCoordinates(Pair<Integer, Integer> xyCoordinates) {
+        this.xyCoordinates = xyCoordinates;
+    }
+
+} // Character (abstract parent)
