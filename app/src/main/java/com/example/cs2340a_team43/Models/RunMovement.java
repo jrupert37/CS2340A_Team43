997758511cc2b90006353cb84e23b2353b5a117e
Jackson Rupert <jrupert37@gatech.edu>
2023-11-05 23:31:07 -0500

    Checkstyle fixes

--- app/src/main/java/com/example/cs2340a_team43/Models/RunMovement.java
+++ app/src/main/java/com/example/cs2340a_team43/Models/RunMovement.java
@@ -4,7 +4,7 @@ package com.example.cs2340a_team43.Models;
 import androidx.core.util.Pair;
 
 public class RunMovement implements MovementBehavior {
-    final int speed = 2;
+    private final int speed = 2;
     public Pair<Integer, Integer> move(Pair<Integer, Integer> xy, MovementDirection direction) {
         int x = xy.first;
         int y = xy.second;
