e9b4226745f18cd4b088857ab163d6a1430d965e
Jackson Rupert <jrupert37@gatech.edu>
2023-11-07 14:13:26 -0500

    Add parameters to update function for purpose of characters notifying observers of their new location

--- app/src/main/java/com/example/cs2340a_team43/Models/Observer.java
+++ app/src/main/java/com/example/cs2340a_team43/Models/Observer.java
@@ -7,5 +7,5 @@ package com.example.cs2340a_team43.Models;
  * This interface is useful for implementing the Observer Design Pattern.
  */
 public interface Observer {
-    public void update();
+    public void update(int x, int y);
 } // Observer (interface)
