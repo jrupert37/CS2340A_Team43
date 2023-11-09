ec2a2409ac2dcf1535e3b82ffdc22e6e6bdc690e
Jackson Rupert <jrupert37@gatech.edu>
2023-11-08 22:34:09 -0500

    Checkstyle fixes

--- app/src/main/java/com/example/cs2340a_team43/ViewModels/PlayerViewModel.java
+++ app/src/main/java/com/example/cs2340a_team43/ViewModels/PlayerViewModel.java
@@ -2,11 +2,11 @@ package com.example.cs2340a_team43.ViewModels;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import com.example.cs2340a_team43.Models.CollisionObserver;
-import com.example.cs2340a_team43.Models.MovementBehavior;
+import com.example.cs2340a_team43.Interfaces.CollisionObserver;
+import com.example.cs2340a_team43.Interfaces.MovementBehavior;
 import com.example.cs2340a_team43.Models.Player;
-import com.example.cs2340a_team43.Models.Subject;
-import com.example.cs2340a_team43.Models.ViewObserver;
+import com.example.cs2340a_team43.Interfaces.Subject;
+import com.example.cs2340a_team43.Interfaces.ViewObserver;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,13 +18,17 @@ public class PlayerViewModel extends CharacterViewModel implements Subject, Coll
     private int initialY;
     private final List<CollisionObserver> collisionObservers;
     private final List<ViewObserver> viewObservers;
-    private boolean notified;
+    private final boolean notified;
+    private int xBound;
+    private int yBound;
 
     private PlayerViewModel() {
         super(Player.getInstance());
         this.player = Player.getInstance();
         collisionObservers = new ArrayList<>();
         viewObservers = new ArrayList<>();
+        xBound = 0;
+        yBound = 0;
         notified = false;
     }
 
@@ -35,8 +39,8 @@ public class PlayerViewModel extends CharacterViewModel implements Subject, Coll
         return playerViewModel;
     }
 
-    public Bitmap getPlayerBitmap() {
-        return this.player.getBitmap();
+    public Bitmap getPlayerSprite() {
+        return this.player.getSprite();
     }
 
     public int getPlayerX() {
@@ -63,9 +67,9 @@ public class PlayerViewModel extends CharacterViewModel implements Subject, Coll
         this.player.setInitialHP(difficulty);
     }
 
-//    public void setPlayerHP(int hp) {
-//        this.player.setHP(hp);
-//    }
+    //    public void setPlayerHP(int hp) {
+    //        this.player.setHP(hp);
+    //    }
 
     public void setInitialPlayerXY(int x, int y) {
         initialX = x;
@@ -88,13 +92,14 @@ public class PlayerViewModel extends CharacterViewModel implements Subject, Coll
     }
 
     public void movePlayerLeft() {
-        if (willCollideWithWall(getPlayerX() - getPlayerSpeed(), getPlayerY())) {
+        if (willBeOutOfBounds(getPlayerX() - getPlayerSpeed(), getPlayerY())
+                || willCollideWithWall(getPlayerX() - getPlayerSpeed(), getPlayerY())) {
             return;
         }
-//        else if (willCollideWithEnemy(getPlayerX() - getPlayerSpeed(), getPlayerY())) {
-//            notifyAllObservers();
-//            return;
-//        }
+        //        else if (willCollideWithEnemy(getPlayerX() - getPlayerSpeed(), getPlayerY())) {
+        //            notifyAllObservers();
+        //            return;
+        //        }
         // otherwise...
         this.player.moveLeft();
         notifyWithPosition();
@@ -102,26 +107,28 @@ public class PlayerViewModel extends CharacterViewModel implements Subject, Coll
     }
 
     public void movePlayerRight() {
-        if (willCollideWithWall(getPlayerX() + getPlayerSpeed(), getPlayerY())) {
+        if (willBeOutOfBounds(getPlayerX() + getPlayerSpeed(), getPlayerY())
+                || willCollideWithWall(getPlayerX() + getPlayerSpeed(), getPlayerY())) {
             return;
         }
-//        else if (willCollideWithEnemy(getPlayerX() + getPlayerSpeed(), getPlayerY())) {
-//            notifyAllObservers();
-//            return;
-//        }
+        //        else if (willCollideWithEnemy(getPlayerX() + getPlayerSpeed(), getPlayerY())) {
+        //            notifyAllObservers();
+        //            return;
+        //        }
         this.player.moveRight();
         notifyWithPosition();
         notifyViewObservers();
     }
 
     public void movePlayerUp() {
-        if (willCollideWithWall(getPlayerX(), getPlayerY() - getPlayerSpeed())) {
+        if (willBeOutOfBounds(getPlayerX(), getPlayerY() - getPlayerSpeed())
+                || willCollideWithWall(getPlayerX(), getPlayerY() - getPlayerSpeed())) {
             return;
         }
-//        else if (willCollideWithEnemy(getPlayerX(), getPlayerY() - getPlayerSpeed())) {
-//            notifyAllObservers();
-//            return;
-//        }
+        //        else if (willCollideWithEnemy(getPlayerX(), getPlayerY() - getPlayerSpeed())) {
+        //            notifyAllObservers();
+        //            return;
+        //        }
         // otherwise...
         this.player.moveUp();
         notifyWithPosition();
@@ -129,13 +136,14 @@ public class PlayerViewModel extends CharacterViewModel implements Subject, Coll
     }
 
     public void movePlayerDown() {
-        if (willCollideWithWall(getPlayerX(), getPlayerY() + getPlayerSpeed())) {
+        if (willBeOutOfBounds(getPlayerX(), getPlayerY() + getPlayerSpeed())
+                || willCollideWithWall(getPlayerX(), getPlayerY() + getPlayerSpeed())) {
             return;
         }
-//        else if (willCollideWithEnemy(getPlayerX(), getPlayerY() + getPlayerSpeed())) {
-//            notifyAllObservers();
-//            return;
-//        }
+        //        else if (willCollideWithEnemy(getPlayerX(), getPlayerY() + getPlayerSpeed())) {
+        //            notifyAllObservers();
+        //            return;
+        //        }
         // otherwise...
         this.player.moveDown();
         notifyWithPosition();
@@ -208,19 +216,17 @@ public class PlayerViewModel extends CharacterViewModel implements Subject, Coll
         this.player.setMovementBehavior(behavior);
     }
 
-    public int getPreviousX() {
-        return this.player.getPreviousX();
-    }
-
-    public int getPreviousY() {
-        return this.player.getPreviousY();
-    }
-
     public boolean isAlive() {
         return getPlayerHP() > 0;
     }
 
+    public void setXYBounds(int x, int y) {
+        this.xBound = x;
+        this.yBound = y;
+    }
 
-
+    public boolean willBeOutOfBounds(int x, int y) {
+        return (x < 0 || x > xBound || y < 0 || y > yBound);
+    }
 } // PlayerViewModel
 
