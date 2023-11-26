785ffb5c41303231d24751a50857a5f104db73e4
Jackson Rupert <jrupert37@gatech.edu>
2023-11-26 15:22:20 -0500

    Create methods for specific related lines of code, display win or lose based on if player was alive at end

--- app/src/main/java/com/example/cs2340a_team43/Views/EndScreenActivity.java
+++ app/src/main/java/com/example/cs2340a_team43/Views/EndScreenActivity.java
@@ -12,22 +12,40 @@ import com.example.cs2340a_team43.Models.Leaderboard;
 import com.example.cs2340a_team43.R;
 
 public class EndScreenActivity extends AppCompatActivity {
+    private int winLoseTextColor;
+    private boolean playerIsAlive;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_end);
 
-        boolean playerIsAlive = getIntent().getBooleanExtra("isAlive", true);
+        playerIsAlive = getIntent().getBooleanExtra("isAlive", true);
 
-        int textColor = playerIsAlive ? 0xF825E138 : Color.RED;
+        // F825E138 is a bright shade of green, to signify a win!
+        winLoseTextColor = playerIsAlive ? 0xF825E138 : Color.RED;
+
+        displayBaseScoreAndBonus();
+        fillInLeaderboard();
+        displayWinLoseBanner();
+        setRestartButton();
+    }
+
+    private void displayBaseScoreAndBonus() {
+        TextView baseScoreTextView = findViewById(R.id.baseScoreTextView);
+        String baseScore = "Base Score: " + getIntent().getIntExtra("baseScore", 0);
+        baseScoreTextView.setText(baseScore);
 
         TextView timeBonusTextView = findViewById(R.id.timeBonusTextView);
         String timeBonus = "Time Score Bonus: " + getIntent().getIntExtra("timeBonus", 0);
-        timeBonusTextView.setTextColor(textColor);
+        timeBonusTextView.setTextColor(winLoseTextColor);
         timeBonusTextView.setText(timeBonus);
+    }
 
+    private void fillInLeaderboard() {
         Leaderboard leaderboard = Leaderboard.getInstance();
         TableLayout leaderTable = findViewById(R.id.leaderTableLayout);
+
         /*
          *  Loop over each row of the xml leaderboard table.
          *  For each row, display the player's name, score, start time, and end time
@@ -56,12 +74,16 @@ public class EndScreenActivity extends AppCompatActivity {
         mostRecentScore.setText(score);
         mostRecentStartTime.setText(leaderboard.getMostRecentAttempt().toString("start"));
         mostRecentEndTime.setText(leaderboard.getMostRecentAttempt().toString("end"));
+    }
 
+    private void displayWinLoseBanner() {
         TextView winLoseTextView = findViewById(R.id.winLoseTextView);
         String winLose = playerIsAlive ? "You Win!" : "You Lose!";
         winLoseTextView.setText(winLose);
-        winLoseTextView.setTextColor(textColor);
+        winLoseTextView.setTextColor(winLoseTextColor);
+    }
 
+    private void setRestartButton() {
         Button restartButton = findViewById(R.id.restartButton);
         restartButton.setOnClickListener(v -> {
             Intent intent = new Intent(EndScreenActivity.this, StartScreenActivity.class);
