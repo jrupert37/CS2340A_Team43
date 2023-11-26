6c25d4ab2f7be27a41f5007b694084a02c04231d
Jackson Rupert <jrupert37@gatech.edu>
2023-11-26 15:36:15 -0500

    Remove floor enumeration, add powerup and key to map

--- app/src/main/java/com/example/cs2340a_team43/Models/Map.java
+++ app/src/main/java/com/example/cs2340a_team43/Models/Map.java
@@ -3,7 +3,6 @@ package com.example.cs2340a_team43.Models;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import com.example.cs2340a_team43.R;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
@@ -17,128 +16,81 @@ import java.util.Scanner;
  * tiles, walls, and an exit for each floor. 
  */
 public class Map {
-
-    // helpful enumeration for distinguishing between floors
-    public enum Floor {
-        FIRST_FLOOR,
-        SECOND_FLOOR,
-        THIRD_FLOOR
-    }
-    
-    // another helpful enumeration for distinguishing between an empty tile, wall, and exit
+    // helpful enumeration for distinguishing between an empty tile, wall, and exit
     public enum MapObject {
         WALL,
         EXIT,
         EMPTY
     }
-    private Floor currentFloor;
-    private Bitmap floorBitmap;
-    private Context context;
+    private final Bitmap floorBitmap;
+    private final PowerUp thisFloorsPowerUp;
+    private final Key thisFloorsKey;
     private MapObject[][] roomLayout;
-    private final int rows;
-    private final int cols;
-    private PowerUp thisFloorsPowerUp;
+    private final int yBound;
+    private final int xBound;
 
-    public Map(Context context, int rows, int cols) {
-        // initialize the currentFloor to First Floor
-        this.rows = rows;
-        this.cols = cols;
-        this.currentFloor = Floor.FIRST_FLOOR;
-        this.context = context;
-        // use createFloor to create the 2d layout array and set the floor background image
-        setFloor(currentFloor);
-    }
 
-    public Map(int rows, int cols) {
-        this.rows = rows;
-        this.cols = cols;
-        this.currentFloor = Floor.FIRST_FLOOR;
-    }
-
-    public Floor getFloor() {
-        return this.currentFloor;
-    }
-    
-    public void setFloor(Floor currentFloor) {
-        this.currentFloor = currentFloor;
-        switch (currentFloor) {
-        case FIRST_FLOOR:
-            this.floorBitmap = BitmapFactory.decodeResource(context.getResources(),
-                                R.drawable.floor1);
-            this.roomLayout = createFloorLayout("first_floor_blueprint.txt");
-            break;
-        case SECOND_FLOOR:
-            this.floorBitmap = BitmapFactory.decodeResource(context.getResources(),
-                                R.drawable.floor2);
-            this.roomLayout = createFloorLayout("second_floor_blueprint.txt");
-            break;
-        case THIRD_FLOOR:
-            this.floorBitmap = BitmapFactory.decodeResource(context.getResources(),
-                    R.drawable.floor3);
-            this.roomLayout = createFloorLayout("third_floor_blueprint.txt");
-            break;
-        default:
-            break;
-        }
+    public Map(Context cont, int imgId, String blueprint, PowerUp pwrUp, Key key, XYPair bounds) {
+        this.floorBitmap = BitmapFactory.decodeResource(cont.getResources(), imgId);
+        this.yBound = bounds.y();
+        this.xBound = bounds.x();
+        createFloorLayout(cont, blueprint);
+        this.thisFloorsPowerUp = pwrUp;
+        this.thisFloorsKey = key;
     }
     
     public Bitmap getFloorBitmap() {
-        return this.floorBitmap;
+        return floorBitmap;
     }
     
     public MapObject[][] getRoomLayout() {
-        return this.roomLayout;
+        return roomLayout;
     }
 
     public void setRoomLayout(MapObject[][] x) {
         this.roomLayout = x;
     }
-    
-    /* 
-    * This method creates the 2d layout array of walls/empty tiles/exit based
-    * on a "blueprint" text file. 
-    * Uses an input stream to read from the text file and create the layout accordingly.
-    */
-    private MapObject[][] createFloorLayout(String pathname) {
+
+    public int getXBound() {
+        return xBound - 1;
+    }
+
+    public int getYBound() {
+        return yBound - 1;
+    }
+
+    public PowerUp getThisFloorsPowerUp() {
+        return thisFloorsPowerUp;
+    }
+
+    public Key getThisFloorsKey() {
+        return thisFloorsKey;
+    }
+
+    private void createFloorLayout(Context context, String pathname) {
         InputStream stream = null;
         try {
-            stream = this.context.getAssets().open(pathname);
+            stream = context.getAssets().open(pathname);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        int index = 0; // "index" refers to the nth character from the text file
-        MapObject[][] layout = new MapObject[rows][cols];
+        int index = 0; // "index" refers to the nth character in the text file
+        MapObject[][] layout = new MapObject[yBound][xBound];
         Scanner streamScanner = new Scanner(stream);
         while (streamScanner.hasNext()) {
             // use "index" to determine the corresponding row and col of the next map object
-            int row = index / cols;
-            int col = index % cols;
+            int row = index / xBound;
+            int col = index % xBound;
             char value = streamScanner.next().charAt(0); // grab the next character
             if (value == 'W') {  // W indicates a wall
                 layout[row][col] = MapObject.WALL;
-            } else if (value == 'N') { // N indicates an empty space 
+            } else if (value == 'N') { // N indicates an empty space
                 layout[row][col] = MapObject.EMPTY;
             } else if (value == 'E') { // E indicates an exit
                 layout[row][col] = MapObject.EXIT;
             }
             index++;
         }
-        return layout;
-    }
-
-    public int getXBound() {
-        return this.cols - 1;
-    }
-
-    public int getYBound() {
-        return this.rows - 1;
-    }
-
-    public PowerUp getThisFloorsPowerUp() {
-        return this.thisFloorsPowerUp;
-    }
-
-    public void setPowerUp(PowerUp powerUp) {
-        this.thisFloorsPowerUp = powerUp;
+        this.roomLayout = layout;
     }
 } // Map
